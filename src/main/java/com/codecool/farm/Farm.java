92552216b5ece95441a680450156a00ac58eed71
GDandris <andris.gede@gmail.com>
2021-03-24 15:34:02 +0100

    tasks done

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,44 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+import com.codecool.farm.animal.Pig;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Farm {
 
+    List<Animal> animals;
+
+    public Farm(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void feedAnimals(){
+        for(Animal animal : animals){
+            animal.feed();
+        }
+    }
+
+    public void butcher(Butcher butcher){
+        animals.removeIf(butcher::canButcher);
+    }
+
+    public boolean isEmpty(){
+        return animals.isEmpty();
+    }
+
+    public List<String> getStatus(){
+        List<String> statuses = new ArrayList<>();
+        for(Animal animal : animals) {
+            String animalName = animal instanceof Pig ? "pig" : "cattle";
+            statuses.add("There is a " + animal.getSize() +" sized "+ animalName +" in the farm.");
+        }
+        return statuses;
+
+    }
 }
--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -2,4 +2,11 @@ package com.codecool.farm.animal;
 
 public abstract class Animal {
 
+    int size = 0;
+
+    public int getSize() {
+        return size;
+    }
+
+    public abstract void feed();
 }
